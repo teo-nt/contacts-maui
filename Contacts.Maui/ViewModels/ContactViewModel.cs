60055a4f21224972c57347767af1f618d3a682bd
Teo <thod_ntonas@hotmail.com>
2024-12-01 18:39:54 +0200

    Finish edit contact ViewModel

--- Contacts.Maui/ViewModels/ContactViewModel.cs
+++ Contacts.Maui/ViewModels/ContactViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Contacts.Maui.Views_Mvvm;
 using Contacts.UseCases.Interfaces;
 using Contact = Contacts.CoreBusiness.Contact;
 
@@ -7,18 +9,33 @@ namespace Contacts.Maui.ViewModels
     public partial class ContactViewModel : ObservableObject
     {
         private readonly IViewContactUseCase viewContactUseCase;
+        private readonly IEditContactUseCase editContactUseCase;
 
         [ObservableProperty]
-        private Contact? _contact;
+        private Contact _contact = new Contact();
 
-        public ContactViewModel(IViewContactUseCase viewContactUseCase)
+        public ContactViewModel(IViewContactUseCase viewContactUseCase, IEditContactUseCase editContactUseCase)
         {
             this.viewContactUseCase = viewContactUseCase;
+            this.editContactUseCase = editContactUseCase;
         }
 
         public async Task LoadContact(int contactId)
         {
-            Contact = await viewContactUseCase.ExecuteAsync(contactId);
+            Contact = await viewContactUseCase.ExecuteAsync(contactId) ?? new Contact();
+        }
+
+        [RelayCommand]
+        public async Task EditContact()
+        {
+            await editContactUseCase.ExecuteAsync(Contact.ContactId, Contact);
+            await Shell.Current.GoToAsync($"//{nameof(ContactsMVVMPage)}");
+        }
+
+        [RelayCommand]
+        public async Task BackToContacts()
+        {
+            await Shell.Current.GoToAsync($"//{nameof(ContactsMVVMPage)}");
         }
     }
 }
