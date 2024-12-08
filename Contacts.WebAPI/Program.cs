d2abfecd2b82922590763e1e357bf3d4dc118a4b
Teo <thod_ntonas@hotmail.com>
2024-12-08 19:36:36 +0200

    Add api CRUD functionality for contacts

--- Contacts.WebAPI/Program.cs
+++ Contacts.WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Contacts.WebAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Contacts.WebAPI
@@ -17,9 +18,42 @@ namespace Contacts.WebAPI
 
             // Configure the HTTP request pipeline.
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+            app.MapGet("/api/contacts", async (ApplicationDbContext db) =>
+            {
+                var contacts = await db.Contacts.ToListAsync();
 
-           
+                return Results.Ok(contacts);
+            });
+
+            app.MapPost("/api/contacts", async (Contact contact, ApplicationDbContext db) =>
+            {
+                db.Contacts.Add(contact);
+                await db.SaveChangesAsync();
+            });
+
+            app.MapPut("/api/contacts/{id}", async (int id, Contact contact, ApplicationDbContext db) =>
+            {
+                var contactToUpdate = await db.Contacts.FindAsync(id);
+                if (contactToUpdate is null) return Results.NotFound();
+
+                contactToUpdate.Name = contact.Name;
+                contactToUpdate.Email = contact.Email;
+                contactToUpdate.Phone = contact.Phone;
+                contactToUpdate.Address = contact.Address;
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            });
+
+            app.MapDelete("/api/contacts/{id}", async (int id, ApplicationDbContext db) =>
+            {
+                var contactToDelete = await db.Contacts.FindAsync(id);
+                if (contactToDelete is null) return Results.NotFound();
+                db.Contacts.Remove(contactToDelete);
+                await db.SaveChangesAsync();
+                return Results.Ok(contactToDelete);
+            });
 
             app.Run();
         }
